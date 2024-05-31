8d54a670751b850092b6e94ce934c2e74cf2820a
idn8k <129896903+idn8k@users.noreply.github.com>
2024-05-31 14:21:28 +0200

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import ColorSquare from './ColorSquare';
-import SquareGen from './SquareGen';
-import Box from './Box';
 import BoxContainer from './BoxContainer';
 import './App.css';
 
